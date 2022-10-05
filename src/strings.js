362aea5425c97cac91969844850ed81fc805e4ff
George Aldington <georgealdington@gmail.com>
2022-10-05 11:25:56 +0100

    used eslint suggestions to tidy code

--- src/strings.js
+++ src/strings.js
@@ -1,26 +1,26 @@
-const sayHello = (string) => {
-  return "Hello, " + string + "!";
+const sayHello = string => {
+  return `Hello, ${string}!`;
 };
 
-const uppercase = (string) => {
+const uppercase = string => {
   return string.toUpperCase();
 };
 
-const lowercase = (string) => {
+const lowercase = string => {
   return string.toLowerCase();
 };
 
-const countCharacters = (string) => {
+const countCharacters = string => {
   return string.length;
 };
 
-const firstCharacter = (string) => {
-  return string.substring(0,1);
+const firstCharacter = string => {
+  return string.substring(0, 1);
 };
 
-function firstCharacters (string, n) {
+function firstCharacters(string, n) {
   return string.substring(0, n);
-};
+}
 
 module.exports = {
   sayHello,
